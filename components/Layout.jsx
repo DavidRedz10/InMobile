13d9fc4a895e8c299e236fb0ba108faab27a85ca
DavidRedz10 <84114450+DavidRedz10@users.noreply.github.com>
2023-01-11 01:30:59 -0500

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,24 @@
 import Head from 'next/head';
-import { Box, layout } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-const Layout = ({ children}) => (
+export default function Layout({ children }) {
+  return (
     <>
       <Head>
         <title>Real Estate</title>
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
-          <Navbar/>
+          <Navbar />
         </header>
         <main>{children}</main>
         <footer>
-          <Footer/>
+          <Footer />
         </footer>
       </Box>
     </>
   );
-
-  export default Layout;
+}
