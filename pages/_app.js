448397b19a521f846aad0c808255fb5bd837ccad
David Redz 10 <xddavidxd2001@gmail.com>
2023-01-10 22:14:33 -0500

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import Router from 'next/router'
+import Head from 'next/head'
+import NProgress from 'nprogress'
+
+import Layout from '../components/Layout';
+
+function MyApp ({ Component, pageProps }) {
+  return(
+    <>
+    <Head>
+
+    </Head>
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps}/>
+      </Layout>
+    </ChakraProvider>
+    </>
+  )
+}
+
+export default MyApp;
