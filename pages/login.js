27c16a6ccb126f00110e7df718e672833a6b8400
David Redz 10 <xddavidxd2001@gmail.com>
2023-04-10 21:21:33 -0500

    Pagina de perfil

--- pages/login.js
+++ pages/login.js
@@ -111,7 +111,7 @@ export default function LoginForm() {
           <Text fontSize={"sm"} align={"center"} color={"gray.500"}>
             Do not have an account?        
           </Text>
-          <Link  align={"center"} color={"blue.400"}> Sign up</Link>
+          <Link  href="/profile" align={"center"} color={"blue.400"}> Sign up</Link>
         </Stack>
       </Box>
     </Flex>
