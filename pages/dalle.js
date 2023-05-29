ff48b78ebe791f35452794ab7f8f19cd392d5273
David Redz 10 <xddavidxd2001@gmail.com>
2023-05-29 00:06:58 -0500

    LOGIN CON GOOGLE Y FOOTER

--- pages/dalle.js
+++ pages/dalle.js
@@ -4,17 +4,15 @@ import { InputBox } from "../components/InputBox";
 import { Button, Heading, Img, Flex } from '@chakra-ui/react'
 
 const configuration = new Configuration({
-    apiKey: process.env.NEXT_PUBLIC_REACT,
+  apiKey: process.env.NEXT_PUBLIC_REACT,
 });
 
 const openai = new OpenAIApi(configuration);
 
-
-
 function App() {
   const [userPrompt, setUserPrompt] = useState("");
-  const [number] = useState(1);
-  const [size, setSize ] = useState ("256x256");
+  const [number, setNumber] = useState(1);
+  const [size, setSize] = useState("256x256");
   const [imageUrl, setImageUrl] = useState("");
 
   const generateImage = async () => {
@@ -23,17 +21,27 @@ function App() {
       n: parseInt(number),
       size: size,
     };
+
     const response = await openai.createImage(imageParameters);
     const urlData = response.data.data[0].url;
     setImageUrl(urlData);
   };
 
   return (
-    <Flex>
-      <Heading marginInline="50px">Describenos tu casa ideal y te planteamos ideas de como luciria!</Heading>
-      {imageUrl && <Img src={imageUrl} className="image" alt="ai thing" />}
+    <Flex direction="column" align="center" justify="center" minHeight="100vh">
+      <Heading marginBottom="2rem">
+        Describenos tu casa ideal y te planteamos ideas de cómo luciría!
+      </Heading>
+      {imageUrl && <Img src={imageUrl} alt="AI generated image" />}
       <InputBox setAttribute={setUserPrompt} />
-      <Button className="main-button" onClick={() => generateImage()}>
+      <Button
+        onClick={generateImage}
+        marginTop="1rem"
+        paddingX="2rem"
+        paddingY="1rem"
+        borderRadius="md"
+        colorScheme="teal"
+      >
         Generar
       </Button>
     </Flex>
